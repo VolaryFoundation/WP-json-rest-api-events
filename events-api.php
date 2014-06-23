778835a8acd71854a949ef6cc690c663614ec2c4
Nick Johannsen <Nicholas.Johannsen@gmail.com>
2014-06-23 15:34:49 -0700

    Added license info

--- events-api.php
+++ events-api.php
@@ -17,3 +17,24 @@ function events_api_init( $server ) {
     $events_api_event->register_filters();
 }
 add_action( 'wp_json_server_before_serve', 'events_api_init' );
+
+//==========================================================================//
+// This file is part of JSON Rest API Events.                               //
+//                                                                          //
+// JSON Rest API Events is Copyright 2014 Volary Foundation and             //
+// Contributors                                                             //
+//                                                                          //
+// JSON Rest API Events is free software: you can redistribute it and/or    //
+// modify it under the terms of the GNU Affero General Public License as    //
+// published by the Free Software Foundation, either version 3 of the       //
+// License, or  at your option) any later version.                          //
+//                                                                          //
+// JSON Rest API Events is distributed in the hope that it will be useful,  //
+// but  WITHOUT ANY WARRANTY; without even the implied warranty of          //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU        //
+// Affero General Public License for more details.                          //
+//                                                                          //
+// You should have received a copy of the GNU Affero General Public         //
+// License along with JSON Rest API Events.  If not, see                    //
+// <http://www.gnu.org/licenses/>.                                          //
+//==========================================================================//
